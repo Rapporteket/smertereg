8181e368411726f290e93f73be1902d9642f4e77
Are Edvardsen <are.edvardsen@helse-nord.no>
2017-11-02 14:58:46 +0100

    available tables

--- R/SmerteGetRegDataFromRapporteketDb.R
+++ R/SmerteGetRegDataFromRapporteketDb.R
@@ -4,7 +4,16 @@ smerteGetRegDataFromRapporteketDb <- function() {
   dbType <- "mysql"
   
   query <- "SELECT
-    FROM"
+    FROM
+    Tables_in_SmerteReportDataStaging
+1                           AlleVar
+2                        AlleVarNum
+3                 Avdelingsoversikt
+4                       Brukerliste
+5                   ForlopsOversikt
+6                    SkjemaOversikt
+7                   SmerteDiagnoser
+8                SmerteDiagnoserNum"
   
   dat <- rapbase::LoadRegData(registryName, query)
   
