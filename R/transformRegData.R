cefa71bd3518cf64d7b9dc385c3f89de06ff0ba9
Are Edvardsen <are.edvardsen@helse-nord.no>
2017-11-17 15:29:19 +0100

    start implementing...

--- R/transformRegData.R
+++ R/transformRegData.R
@@ -8,14 +8,34 @@
 #'
 #' @param dat a data frame containing original data
 #' @param meta a data frame describing the each column in the original data
+#' @param is_var_lang_no logical TRUE if norwegian names (default)
 #'
 #' @return a data frame of transformed data
 #' @export
 #'
 #' @examples
 
-transformRegData <- function(dat, meta) {
-  # what we need to do
+transformRegData <- function(dat, meta, is_var_lang_no = TRUE) {
   
-  # 
+  # check for duplicates
+  if (any(duplicated(names(dat)))) {
+    stop("Duplicate(s) found:\n",
+         stringr::str_c(names(dat)[duplicated(names(dat))]),
+         "Stopping",
+         collapse = "\n")
+  }
+  
+  # names language
+  if (is_var_lang_no) {
+    dd_kolid = "oqr_variabel_id_norsk"
+  } else {
+    dd_kolid = "oqr_variabel_id_engelsk"
+  }
+  
+  # meta names
+  varnamn <- meta$variabel_id[match(names(dat), meta[[dd_kolid]])] %>%
+    dplyr::coalesce(names(dat))
+  
+  # meta entry ids
+  kb_info = meta %>% distinct(variabel_id, .keep_all = TRUE)
 }
