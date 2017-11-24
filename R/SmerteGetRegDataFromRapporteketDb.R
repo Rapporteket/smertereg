d8cb28a62c720c4ac5fb55ae4cd9afe48747fcd5
Are Edvardsen <are.edvardsen@helse-nord.no>
2017-11-24 10:30:32 +0100

    giving up, try to adapt to whatever needed by the report...

--- R/SmerteGetRegDataFromRapporteketDb.R
+++ R/SmerteGetRegDataFromRapporteketDb.R
@@ -10,17 +10,17 @@ smerteGetRegDataFromRapporteketDb <- function() {
   
   query <- "
   SELECT
-    PasientID,
-    ForlopsID,
-    AntTilsLege,
-    AntTilsSykPleier,
-    AntTilsFysioT,
-    AntTilsPsyk,
-    AntTilsSosio,
-    Tilsett,
-    InnlAvd,
-    AntPasTils,
-    StartdatoTO,
+    PasientID as oqr_pasientid,
+    ForlopsID as oqr_forlopsid,
+    AntTilsLege as emp22_caregiver_doctor_times,
+    AntTilsSykPleier as emp22_caregiver_nurse_times,
+    AntTilsFysioT as emp22_caregiver_physiotherapist_times,
+    AntTilsPsyk as emp22_caregiver_psychologist_times,
+    AntTilsSosio as emp22_caregiver_social_worker_times,
+    Tilsett as mce_supervision,
+    InnlAvd as emp11_department,
+    AntPasTils as emp22_consultations,
+    StartdatoTO as mce_registered_date,
     AvdRESH
   FROM
     AlleVarNum
@@ -28,8 +28,9 @@ smerteGetRegDataFromRapporteketDb <- function() {
   
   dat <- rapbase::LoadRegData(registryName, query)
   
-  dat$StartdatoTO <- lapply(dat$StartdatoTO, readr::parse_datetime,
-                            format = "%Y-%m-%d")
+  dat$mce_registered_date <- lapply(dat$mce_registered_date,
+                                    readr::parse_datetime,
+                                    format = "%Y-%m-%d")
   
   return(dat)
   
