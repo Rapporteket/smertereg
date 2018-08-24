d33381aaf1c9b8bb4d02d552685dbb43c1dbbe8f
Are Edvardsen <are.edvardsen@helse-nord.no>
2018-08-24 14:05:03 +0200

    init commit query for local reports (tilsyn)

---
+++ R/GetRegDataTilsyn.R
@@ -0,0 +1,40 @@
+#' Query registry data from smerte db for tilsyn
+#'
+#' @param reshID string providing dept key
+#' @return a data frame with registry data
+#' @export
+
+getRegDataTilsyn <- function(reshID) {
+  
+  registryName <- "smerte"
+  dbType <- "mysql"
+  
+  query <- "
+  SELECT
+    PasientID as oqr_pasientid,
+    ForlopsID as oqr_forlopsid,
+    AntTilsLege as emp22_caregiver_doctor_times,
+    AntTilsSykPleier as emp22_caregiver_nurse_times,
+    AntTilsFysioT as emp22_caregiver_physiotherapist_times,
+    AntTilsPsyk as emp22_caregiver_psychologist_times,
+    AntTilsSosio as emp22_caregiver_social_worker_times,
+    Tilsett as mce_supervision,
+    InnlAvd as emp11_department,
+    AntPasTils as emp22_consultations,
+    StartdatoTO as mce_registered_date,
+    AvdRESH
+  FROM
+    AlleVarNum
+"
+  
+  query <- paste(query, "WHERE AvdRESH IN (", reshID, ")")
+  
+  
+  dat <- rapbase::LoadRegData(registryName, query)
+  
+  dat$mce_registered_date <- readr::parse_datetime(dat$mce_registered_date,
+                                                   format = "%Y-%m-%d")
+  
+  return(dat)
+  
+}
